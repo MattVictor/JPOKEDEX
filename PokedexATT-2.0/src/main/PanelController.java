27b8176a285201abbdc94625dc667b168f542493
Matheus Victor <119546976+MattVictor@users.noreply.github.com>
2024-04-16 18:47:21 -0300

    Delete PokedexATT-2.0 directory

--- PokedexATT-2.0/src/main/PanelController.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public enum PanelController {
-	
-	INITIAL_PANEL, POKEDEX_PANEL, CHART_PANEL, POKEMON_DETAILS_PANEL;
-	
-	public static PanelController currentPanel = INITIAL_PANEL;
-}
